3751bb7552e7433ccc1734fd9122257cd2f05d48
刘鸿儒 <liuhongru@liquidnetwork.com>
2019-08-22 12:09:29 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name = "liquid_k8s_yaml_generator",
     version = "0.1",
     packages = ['liquid_k8s_yaml_generator'],
-    data_files=['liquid_k8s_yaml_generator/templates/test_tpl.tmpl'],
+    data_files=[('templates',['liquid_k8s_yaml_generator/templates/test_tpl.tmpl'])],
     install_requires = [
     'jinja2>=2.10.1'
     ]
---
+++ test.py
@@ -0,0 +1,14 @@
+import liquid_k8s_yaml_generator
+
+
+
+yaml_fb = liquid_k8s_yaml_generator.template.render(
+image_name="test-demo", #被应用于作为服务、istio vs、应用名、sls 的名字的生成 禁止下划线
+replicas=2, 
+remote_image_name="ubuntu:latest" , # 镜像地址
+port = 80, #应用开放的端口
+env_items = {"KEY":"vaule"}, #环境变量
+log_path = "/log/*log"  #sls 采集日志的地址
+)
+
+print(yaml_fb)
