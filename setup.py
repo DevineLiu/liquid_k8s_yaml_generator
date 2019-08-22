610f10780078bafa74daf860e5603728e5e8b100
刘鸿儒 <liuhongru@liquidnetwork.com>
2019-08-22 12:26:33 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name = "liquid_k8s_yaml_generator",
     version = "0.1",
     packages = ['liquid_k8s_yaml_generator'],
-    data_files=[('templates',['liquid_k8s_yaml_generator/templates/test_tpl.tmpl'])],
+    package_dir={'liquid_k8s_yaml_generator':'liquid_k8s_yaml_generator/templates'},
     install_requires = [
     'jinja2>=2.10.1'
     ]
